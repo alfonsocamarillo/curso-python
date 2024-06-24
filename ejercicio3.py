8d137204fd1acce4e5ef9e8046eabc5782351280
alfonsocamarillo <138623361+alfonsocamarillo@users.noreply.github.com>
2024-06-24 12:06:05 -0600

    prueba

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,9 +2,11 @@ frase = input("Ingrese un texto: ")
 boleano = input("Ingrese un True O False: ")
 entero = input("Ingrese un numero entero: ")
 decimal = input("Ingrese un numero desimal: ")
+
 boleano=bool(boleano)
 entero=int(entero)
 decimal=float(decimal)
+
 print(frase, "es: " , type(frase))
 print(boleano, "es: " , type(boleano))
 print(entero, "es: " , type(entero))
