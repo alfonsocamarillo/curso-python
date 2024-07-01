f239973a2937db01ccfe0ffa1335d782343afd8c
alfonsocamarillo <138623361+alfonsocamarillo@users.noreply.github.com>
2024-07-01 10:29:23 -0600

    Ejercicios_condicionales

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,6 @@
-print("Ingrese 4 palabras")
-palabra1 = input("Ingrese la primera palabra: ")
-palabra2 = input("Ingrese la segunda palabra: ")
-palabra3 = input("Ingrese la tercera palabra: ")
-palabra4 = input("Ingrese la cuarta palabra: ")
-
-print(palabra1==palabra2 or palabra3!=palabra4)
+texto = input("Ingrese un texto: ")
+palabra = input("Ingrese la palabra a buscar en el texto: ")
+if palabra in texto:
+    print("Esa palabra esta en el texto")
+else:
+    print("Esa palabra no existe en este texto")
